38cf77afea8544d7737a6840fc50be21ea1db5bb
Anonymous_N <nam348tnh.3gp@gmail.com>
2026-05-10 02:28:38 +0700

    Update run.sh

--- Backup/run.sh
+++ Backup/run.sh
@@ -1,7 +1,7 @@
 apt update -y
-apt install nodejs npm git -y
+apt install nodejs npm -y
 git clone https://github.com/nam348tnh3gp/chocohub
-cd chocohub
-cd Backup
+cd chocohub/Backup
+npm cache clean --force
 npm install better-sqlite3 dotenv
 node backup.js
